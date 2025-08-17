37420e0603210544628a4c5e3fd055c622a7ae88
Aman Kumar Mishra <aman@Amans-MacBook-Air.local>
2025-08-18 00:05:33 +0530

    db setup/models/err middleware

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middleware/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({
+  extended
+    : false
+}))
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
+
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
   res.send('Welecome tot the Subscription Tracker API!');
@@ -18,6 +30,8 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Subscription Tracker API is running on http://localhost:${PORT}`)
+
+  connectToDatabase()
 })
 
 export default app;
